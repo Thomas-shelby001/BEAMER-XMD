a7596dd85f704b234f1e1532d873f6e378b192c4
Sang Lee😁 <leesang9146@gmail.com>
2026-04-12 11:30:38 +0300

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,4 +1,4 @@
 module.exports = {
-    ownerNumber: [''], // Put your number here e.g. ['2547XXXXXXXX']
+    ownerNumber: ['254778821373'], // Put your number here e.g. ['2547XXXXXXXX']
     sessionID: ''      // Paste your session ID here
 };
